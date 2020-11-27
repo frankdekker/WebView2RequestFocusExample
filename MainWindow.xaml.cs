7bf72cf0a8c9e84862fb5804a6fdd9f16789836e
Frank Dekker <fdekker@123inkt.nl>
2020-11-27 20:30:28 +0100

    WebView2RequestFocus example

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WebView2RequestFocusExample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is TabControl tabControl)
+            {
+                Debug.WriteLine("Selected index:" + tabControl.SelectedIndex);
+                if (tabControl.SelectedIndex == 0)
+                {
+                    WebViewA.Focus();
+                }
+                else if (tabControl.SelectedIndex == 1)
+                {
+                    WebViewB.Focus();
+                }
+            }
+        }
+    }
+}
